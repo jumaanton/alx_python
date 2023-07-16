8f17629095e1b59324edc1b29a89784903b27212
Juma Antony <jumaantony840@gmail.com>
2023-07-16 16:07:31 +0300

    completed task 3

--- python-hello_world/3-last_digit.py
+++ python-hello_world/3-last_digit.py
@@ -7,6 +7,6 @@ if last_digit > 5:
     output += " and is greater than 5"
 elif last_digit == 0:
     output += " and is 0"
-else:
+elif last_digit < 6 and last_digit != 0:
     output += " and is less than 6 and not 0"
 print(output)
