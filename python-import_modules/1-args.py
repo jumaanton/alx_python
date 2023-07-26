a2ac1da49afa3033b7b3c3f8828b4fdafb98ec36
Juma Antony <jumaantony840@gmail.com>
2023-07-26 18:14:09 +0300

    completed task 1

---
+++ python-import_modules/1-args.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+import sys
+
+if __name__ == "__main__":
+    argv = sys.argv[1:]  # Exclude the script name from the arguments
+
+    num_arguments = len(argv)
+
+    if num_arguments == 0:
+        print("0 arguments.")
+    else:
+        print(f"{num_arguments} argument{'s' if num_arguments > 1 else ''}:")
+        for i, arg in enumerate(argv, 1):
+            print(f"{i}: {arg}")
+
