e4fba3b08c687b0597bec36e4b4024b6a7cd02c7
Juma Antony <jumaantony840@gmail.com>
2023-07-26 18:07:09 +0300

    tweaked task 1

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
-add = __import__('add_0').add
 
-a = 1
-b = 2
-
-print(f"{a} + {b} = {add(a, b)}")
-
+if __name__ == "__main__":
+    from add_0 import add
+    
+    a = 1
+    b = 2
+    
+    result = add(a, b)
+    
+    print(f"{a} + {b} = {result}")
