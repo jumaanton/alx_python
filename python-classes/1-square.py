e4e57163882a9076cdeb118f9c5828e59ed03ea4
Juma Antony <jumaantony840@gmail.com>
2023-08-02 08:51:55 +0300

    task 1

---
+++ python-classes/1-square.py
@@ -0,0 +1,21 @@
+""" 
+This is a Square module.
+"""
+
+class Square:
+    """
+    A class that defines a square by size.
+    """
+    
+    def __init__(self, size=0):
+        self.__size = 0  # Default size is 0
+
+        # Check if size is provided and is an integer
+        if isinstance(size, int):
+            # Check if size is non-negative
+            if size >= 0:
+                self.__size = size
+            else:
+                raise ValueError("size must be >= 0")
+        else:
+            raise TypeError("size must be an integer")
