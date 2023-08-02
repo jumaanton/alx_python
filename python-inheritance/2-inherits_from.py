0181639a437384e1db14f0a106f2648e158ca3aa
Juma Antony <jumaantony840@gmail.com>
2023-08-02 11:08:55 +0300

    tweaked task 2

--- python-inheritance/2-inherits_from.py
+++ python-inheritance/2-inherits_from.py
@@ -3,10 +3,17 @@
  of a class that inherited (directly/indirectly)
 """
 
-
 def inherits_from(obj, a_class):
-    """ 
-        my func definition 
+    """
+    Return True if an object is an instance
+    of a class that inherited (directly or indirectly).
+
+    Args:
+        obj (object): The object to check.
+        a_class (class): The specified class to check against.
+
+    Returns:
+        bool: True if the object is an instance of a subclass of the specified class,
+              otherwise False.
     """
     return issubclass(type(obj), a_class) and type(obj) is not a_class
-    
