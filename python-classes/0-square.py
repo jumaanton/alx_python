f3882b2967a2b01b0798abdb00cda683e1a08ca2
Juma Antony <jumaantony840@gmail.com>
2023-08-02 08:46:47 +0300

    tweaked task 0

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,11 +1,11 @@
+#!/usr/bin/python3
 """
 This is the "Square"  module.
 
-This module provides a simple Square class.
+This module provides a simple Square class with initialize size.
 """
 
 
 class Square:
     def __init__(self, size):
         self.__size = size
-        
