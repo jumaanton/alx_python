6eb220b96c1e5e240188d603fdeec1e862a859f9
Juma Antony <jumaantony840@gmail.com>
2023-08-02 11:30:08 +0300

    task 7

---
+++ python-inheritance/7-rectangle.py
@@ -0,0 +1,29 @@
+""" Rectangle module """
+
+class BaseGeometry:
+    """ BaseGeometry class """
+    def area(self):
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        if not isinstance(value, int):
+            raise TypeError(f"{name} must be an integer")
+        if value <= 0:
+            raise ValueError(f"{name} must be greater than 0")
+
+""" Module that defines a class Rectangle that inherits from BaseGeometry"""
+
+class Rectangle(BaseGeometry):
+    """ Rectangle class """
+    
+    def __init__(self, width, height):
+        self.__width = width
+        self.__height = height
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+
+    def area(self):
+        return self.__width * self.__height
+
+    def __str__(self):
+        return f"[Rectangle] {self.__width}/{self.__height}"
