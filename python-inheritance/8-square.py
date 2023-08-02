beb11cbe41291977f9e9630c5f1b8f986228f823
Juma Antony <jumaantony840@gmail.com>
2023-08-02 11:33:21 +0300

    task 7

---
+++ python-inheritance/8-square.py
@@ -0,0 +1,42 @@
+""" Module that defines a class Square that inherits from Rectangle"""
+
+class BaseGeometry:
+    """ BaseGeometry class """
+
+    def area(self):
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        if not isinstance(value, int):
+            raise TypeError(f"{name} must be an integer")
+        if value <= 0:
+            raise ValueError(f"{name} must be greater than 0")
+
+""" Module that defines a class Rectangle that inherits from BaseGeometry"""
+
+class Rectangle(BaseGeometry):
+    """ Rectangle class """
+
+    def __init__(self, width, height):
+        self.__width = width
+        self.__height = height
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+
+    def area(self):
+        return self.__width * self.__height
+
+    def __str__(self):
+        return f"[Rectangle] {self.__width}/{self.__height}"
+
+""" Module that defines a class Square that inherits from Rectangle"""
+
+class Square(Rectangle):
+    """ Square class """
+    
+    def __init__(self, size):
+        super().__init__(size, size)
+        self.integer_validator("size", size)
+
+    def __str__(self):
+        return f"[Square] {self._Rectangle__width}/{self._Rectangle__height}"
