f53836d21e95fad34f4caf83e9284f1e03d43b7b
Juma Antony <jumaantony840@gmail.com>
2023-08-23 13:30:43 +0300

    fixing task 4 error

--- python-object_relational_mapping/4-cities_by_state.py
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -22,7 +22,8 @@ def main():
 
         # Execute the SQL query
         cursor.execute(
-            "SELECT cities.id, cities.name, states.name FROM cities ORDER BY id ASC")
+            "SELECT cities.id, cities.name, cities.state_id.name FROM cities\
+				 ORDER BY id ASC")
 
         # Fetch all the rows
         rows = cursor.fetchall()
