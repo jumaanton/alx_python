47b77499bd721e805b7f94d1556d581e6320cf49
Juma Antony <jumaantony840@gmail.com>
2023-08-23 12:53:04 +0300

    task 4

---
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+import sys
+import MySQLdb
+
+
+def main():
+    if len(sys.argv) != 4:
+        print("Usage: {} <username> <password> <database>".format(sys.argv[0]))
+        sys.exit(1)
+
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+
+    try:
+        # Connect to MySQL server
+        db = MySQLdb.connect(host="localhost", port=3306,
+                             user=username, passwd=password, db=database)
+
+        # Create a cursor
+        cursor = db.cursor()
+		
+		# Execute the SQL query
+        cursor.execute("SELECT * FROM cities ORDER BY id ASC")
+
+
+        # Fetch all the rows
+        rows = cursor.fetchall()
+
+        # Display the results
+        for row in rows:
+            print(row)
+
+    except MySQLdb.Error as e:
+        print("Error connecting to the database:", e)
+    finally:
+        # Close the database connection
+        if db:
+            db.close()
+
+
+if __name__ == "__main__":
+    main()
