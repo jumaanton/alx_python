8d70dca96c028fb4ab97b171087800c3da537aa6
Juma Antony <jumaantony840@gmail.com>
2023-08-28 15:56:22 +0300

    documenting the module

--- python-web_framework/0-hello_route.py
+++ python-web_framework/0-hello_route.py
@@ -1,10 +1,18 @@
+# Import the Flask class from the flask module
 from flask import Flask
 
+# Create an instance of the Flask class and pass in the __name__ of the current module
 app = Flask(__name__)
 
-@app.route('/', strict_slashes=False)
-def hello_hbnb():
-    return "Hello HBNB!"
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+# Define a route for the root URL ("/") and set strict_slashes=False to allow both with and without trailing slashes
+@app.route("/", strict_slashes=False)
+def hello_hbnb():
+    # This function will be executed when the root URL is accessed
+    return "Hello HBNB!"  # Return the string "Hello HBNB!" as the response
+
+
+# This block of code will only run if the script is executed directly (not imported as a module)
+if __name__ == "__main__":
+    # Start the Flask development server with the specified host and port
+    app.run(host="0.0.0.0", port=5000)
