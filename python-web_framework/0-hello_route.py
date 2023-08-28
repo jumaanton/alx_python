3e3c8cddb0d33c1e5a9b710fe84f2668b9d89ec5
Juma Antony <jumaantony840@gmail.com>
2023-08-28 16:58:42 +0300

    tweaked task 0

--- python-web_framework/0-hello_route.py
+++ python-web_framework/0-hello_route.py
@@ -1,19 +1,14 @@
-"""Import the Flask class from the flask module"""
+"""Script that starts a Flask web application & displays a value(str)"""
+
+
 from flask import Flask
 
-"""Create an instance of the Flask class and pass in the __name__
-of the current module"""
 app = Flask(__name__)
 
-"""Define a route for the root URL ("/") and set strict_slashes=False to allow
-both with and without trailing slashes"""
-@app.route('/', strict_slashes=False)
-def hello_hbnb():
-    """this function will be executed when the root URL is accessed """
-    return "Hello HBNB!"  # Return the string "Hello HBNB!" as the response
 
- """This block of code will only run if the script is executed directly
-(not imported as a module)"""
+@app.route('/', strict_slashes=False)
+def index():
+    return 'Hello HBNB!'
+
 if __name__ == '__main__':
-    """Start the Flask development server with the specified host and port"""
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5000, debug=True)
