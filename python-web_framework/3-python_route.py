09d833475f1cb8bdeba61b49bf9a34c5c1bce5ef
Juma Antony <jumaantony840@gmail.com>
2023-08-28 17:08:38 +0300

    task 3

---
+++ python-web_framework/3-python_route.py
@@ -0,0 +1,30 @@
+"""Script that starts a Flask web application & displays a value(str)"""
+
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def index():
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+	return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c(text):
+	return 'C {}'.format(text.replace('_', ' '))
+
+
+@app.route('/python/', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def python(text='is cool'):
+	return 'Python {}'.format(text.replace('_', ' '))
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)
